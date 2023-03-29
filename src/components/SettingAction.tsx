0945fd37a06e2c2fb734044ae9d915303af2a5c3
tuqi <QWer123$>
2023-03-29 11:03:46 +0800

    关闭记录连续对话的能力

--- src/components/SettingAction.tsx
+++ src/components/SettingAction.tsx
@@ -74,7 +74,7 @@ export default function SettingAction(props: {
             }}
           />
         </SettingItem>
-        <SettingItem
+        {/* <SettingItem
           icon="i-carbon:save-image"
           label="记录对话内容，刷新不会消失"
         >
@@ -92,7 +92,7 @@ export default function SettingAction(props: {
             />
             <div class="w-9 h-5 bg-slate bg-op-15 peer-focus:outline-none peer-focus:ring-0  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-slate"></div>
           </label>
-        </SettingItem>
+        </SettingItem> */}
         <SettingItem
           icon="i-carbon:3d-curve-auto-colon"
           label="开启连续对话，将加倍消耗 Token"
